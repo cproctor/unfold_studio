8ef5e25b8efb4a7f0f2e5b1fd1f32d3233896fc7
Chris Proctor <chris.proctor@gmail.com>
2017-10-16 12:45:17 -0400

    Added profile model (#7)

---
+++ profiles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):    
+    user = models.OneToOneField('auth.User', related_name='profile')
+    birth_month = models.DateField()
+    gender = models.CharField(max_length=100)
+
--- unfold_studio/settings_default.py
+++ unfold_studio/settings_default.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'unfold_studio'
+    'unfold_studio',
+    'profiles'
 ]
 
 MIDDLEWARE_CLASSES = [
