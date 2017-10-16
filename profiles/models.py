a955338f30983c96a72f61c6d4a146bc2c0e4830
Chris Proctor <chris.proctor@gmail.com>
2017-10-16 13:50:12 -0400

    Added followers to profiles (#13)

--- profiles/models.py
+++ profiles/models.py
@@ -5,4 +5,5 @@ class Profile(models.Model):
     user = models.OneToOneField('auth.User', related_name='profile')
     birth_month = models.DateField()
     gender = models.CharField(max_length=100)
+    following = models.ManyToManyField('profiles.Profile', related_name='followers')
 
