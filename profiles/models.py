64e6cc14ad7529bc9d6612ec7d8e5fe8a313fd58
Chris Proctor <chris.proctor@gmail.com>
2017-10-17 09:12:19 -0400

    Adding events index

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+import arrow
 
 # Create your models here.
 class Profile(models.Model):    
     user = models.OneToOneField('auth.User', related_name='profile')
     birth_month = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=100, null=True, blank=True)
-    following = models.ManyToManyField('profiles.Profile', related_name='followers', null=True, blank=True)
+    following = models.ManyToManyField('profiles.Profile', related_name='followers', blank=True)
 
     def __str__(self):
         return self.user.username
@@ -41,8 +42,30 @@ class Event(models.Model):
     book = models.ForeignKey('unfold_studio.Book', null=True)
     story = models.ForeignKey('unfold_studio.Story', null=True)
 
+    def __str__(self):
+        prefix = "[{}] ".format(self.user)
+        ts = " ({})".format(arrow.get(self.timestamp).format('H:mm a, MMM DD, YYYY'))
+        if self.event_type == Event.LOVED_STORY:
+            body = "{} loved story {}".format(self.subject, self.story.title)
+        elif self.event_type == Event.COMMENTED_ON_STORY:
+            body =  "{} commented on '{}'".format(self.subject, self.story.title)
+        elif self.event_type == Event.FORKED_STORY:
+            body = "{} forked '{}'".format(self.subject, self.story.title)
+        elif self.event_type == Event.PUBLISHED_STORY:
+            body = "{} published '{}'".format(self.subject, self.story.title)
+        elif self.event_type == Event.PUBLISHED_BOOK:
+            body = "{} published book '{}'".format(self.subject, self.book)
+        elif self.event_type == Event.ADDED_STORY_TO_BOOK:
+            body = "{} added '{}' to '{}'".format(self.subject, self.story.title, self.book)
+        elif self.event_type == Event.FOLLOWED:
+            body = "{} now follows you".format(self.subject)
+        elif self.event_type == Event.YOU_FOLLOWED:
+            body = "You now follow {}".format(self.subject)
+        return prefix + body + ts
+
     class Meta:
         # Ensures no duplication of events
-        unique_together = (('user', 'event_type', 'subject', 'book', 'story'),)
+        unique_together = ('user', 'event_type', 'subject', 'book', 'story')
+        indexes = [models.Index(fields=['user', 'timestamp'])]
         ordering = ('timestamp',)
 
