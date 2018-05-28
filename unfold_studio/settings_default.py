ca763eef36ae94c7a6a865990967f62d6589d363
Chris Proctor <chris.proctor@gmail.com>
2018-05-27 21:35:35 -0400

    Adding sites framework

--- unfold_studio/settings_default.py
+++ unfold_studio/settings_default.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+SITE_ID = 1
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'reversion',
     'unfold_studio',
     'profiles'
