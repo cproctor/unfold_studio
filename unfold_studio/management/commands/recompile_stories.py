22417f094ea7115d5281c816770f769c4683c6e4
Chris Proctor <chris.proctor@gmail.com>
2018-10-23 10:39:59 -0400

    Updating recompile management command

--- unfold_studio/management/commands/recompile_stories.py
+++ unfold_studio/management/commands/recompile_stories.py
@@ -1,13 +1,25 @@
 from django.core.management.base import BaseCommand, CommandError
 from unfold_studio.models import Story
+import logging
+import reversion
+
+log = logging.getLogger(__name__)    
 
 class Command(BaseCommand):
     help = "Recompile all stories (for example after upgrading inklecate)"
 
     def handle(self, *args, **options):
         for story in Story.objects.all():
-            print("Recompiling '{}' (Story {})".format(story, story.id))
-            story.compile_ink()
-            story.save()
+            log.debug("Recompiling '{}' (Story {})".format(story, story.id))
+            if not story.latest_version():
+                with reversion.create_revision():
+                    story.save()
+                    reversion.set_user(story.author)
+                    reversion.set_comment("Initial version created by management script")
+            story.compile()
+            with reversion.create_revision():
+                story.save()
+                reversion.set_user(story.author)
+                reversion.set_comment("Story recompiled by management script")
 
 
