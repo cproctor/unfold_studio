77e4ef4452d6cd6d8ff2d4b163e77abaa10adc0b
Chris Proctor <chris.proctor@gmail.com>
2018-10-28 11:48:16 -0400

    Added parent option to top_stories command

--- unfold_studio/management/commands/top_stories.py
+++ unfold_studio/management/commands/top_stories.py
@@ -11,28 +11,33 @@ class Command(BaseCommand):
         parser.add_argument("-n", '--number_of_stories', type=int, default=20)
         parser.add_argument("-i", '--integers', action='store_true')
         parser.add_argument("-l", '--long_headers', action='store_true')
+        parser.add_argument("-p", '--parent', type=int, help="Show only stories forked from parent")
         parser.add_argument("-o", '--outfile')
 
     def handle(self, *args, **options):
         props = lambda s: (s.id, s.title, 
             s.author.username if s.author else "", s.priority, s.score(), 
-            s.age_in_hours(), s.loves.count(), s.books.count(), 
+            s.age_in_hours(), int(s.shared or s.public), s.loves.count(), s.books.count(), 
             s.children.filter(~Q(author=s.author)).count(), s.includes.count(),
             s.included_by.count(), int(s.errors.exists()), int(s.featured))
         zerosToDots = lambda v: '.' if v == 0 else v
-        stories = map(props, Story.objects.all()[:options['number_of_stories']])
+        stories = Story.objects.all()
+        if options['parent']:
+            stories = stories.filter(parent_id=options['parent'])
+        stories = stories[:options['number_of_stories']]
+        stories = map(props, stories)
         if not options['integers']:
             stories = map(lambda story: [zerosToDots(s) for s in story], stories)
         stories = list(stories)
         if options['long_headers']:
             headers = [
                 "id", "title", "author", "priority", "score", "age_hours", 
-                "loves", "books", "children", "includes",
+                "shared", "loves", "books", "children", "includes",
                 "included_by", "errors", "featured"
             ]
         else:
             headers = [
-                "id", "title", "auth", "pri", "sco", "age", "<3", 
+                "id", "title", "auth", "pri", "sco", "age", "sh", "<3", 
                 "bks", "chld", "inc", "inc_by", "err", "ftd"
             ]
         if options['outfile']:
