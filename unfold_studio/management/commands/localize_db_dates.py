e05042c91d36e5bdcafe444738bfe7014fb8d0c0
Chris Proctor <chris.proctor@gmail.com>
2019-03-14 21:49:46 -0400

    Adding db date localize task

---
+++ unfold_studio/management/commands/localize_db_dates.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+import pytz
+from unfold_studio.models import Story
+from profiles.models import Event
+
+class Command(BaseCommand):
+    help = "Localize all datetimes in the database as UTC"
+
+    def handle(self, *args, **kwargs):
+        tz = pytz.timezone('utc')
+        for s in Story.objects.all():
+            if not s.creation_date.tzinfo:
+                s.creation_date = tz.localize(s.creation_date)
+            if not s.edit_date.tzinfo:
+                s.edit_date = tz.localize(s.edit_date)
+            s.save()
+        for e in Event.objects.all():
+            if not e.timestamp.tzinfo:
+                e.timestamp = tz.localize(e.timestamp)
+                e.save()
