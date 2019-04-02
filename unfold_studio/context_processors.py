555f85aac0c99c4f6e6589071974041aa918d75e
Chris Proctor <chris.proctor@gmail.com>
2019-04-02 09:30:14 -0400

    Passing DEBUG flag to template context; menu bar is red in local debug mode

--- unfold_studio/context_processors.py
+++ unfold_studio/context_processors.py
@@ -4,5 +4,6 @@ def documentation_urls(request):
     return {
         'DOCUMENTATION_URL': settings.DOCUMENTATION_URL,
         'HELP_URL': settings.HELP_URL,
-        'TEACHING_URL': settings.TEACHING_URL
+        'TEACHING_URL': settings.TEACHING_URL,
+        'DEBUG': settings.DEBUG
     }
