5c4a1ad1f4877b69637598fe5ecafe17df0fb9c0
Chris Proctor <chris.proctor@gmail.com>
2019-05-06 17:58:49 -0400

    Timezone issue in revisions

--- unfold_studio/views.py
+++ unfold_studio/views.py
@@ -33,6 +33,7 @@ from literacy_events.models import LiteracyEvent
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from comments.models import Comment
 from comments.forms import CommentForm
+from django.utils import timezone
 
 log = logging.getLogger(__name__)    
 
@@ -410,6 +411,12 @@ class StoryVersionListView(DetailView):
         story = self.get_object()
         versions = Version.objects.get_for_object(story).exclude(revision__comment__exact='').reverse().annotate(
                 index=Window(RowNumber()))
+        for v in versions:
+            r = v.revision
+            if timezone.is_naive(r.date_created):
+                r.date_created = timezone.make_aware(r.date_created)
+                r.save()
+
         comments = Comment.objects.for_story(story).all()
 
         def date(e):
