0fd9dd801c3db7b797a34669773710759131493a
Chris Proctor <chris.proctor@gmail.com>
2019-05-06 17:12:40 -0400

    Admin changes will no longer have revision comments

--- unfold_studio/admin.py
+++ unfold_studio/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 from reversion.admin import VersionAdmin
 from django.db.models import Count
 
+class NoCommentVersionAdmin(VersionAdmin):
+    "Subclass of VersionAdmin which doesn't pollute the revision comments."
+    def log_addition(self, request, object, change_message=None):
+        admin.ModelAdmin.log_addition(request, object, change_message)
+
+    def log_change(self, request, object, message):
+        super().log_change(request, object, '')
+
 @admin.register(Story)
-class StoryAdmin(VersionAdmin):
+class StoryAdmin(NoCommentVersionAdmin):
     list_display = [
         'id',
         'title',
