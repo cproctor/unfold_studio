fd8b5dd4fbd7106e5f86eef1f56a778b20077a76
Chris Proctor <chris.proctor@gmail.com>
2019-05-06 20:52:27 -0400

    Fixed template tags and applying in more places

--- unfold_studio/templatetags/linkrefs.py
+++ unfold_studio/templatetags/linkrefs.py
@@ -1,5 +1,5 @@
 from django import template
-from unfold_studio.models import Story
+from unfold_studio.models import Story, Book
 from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
@@ -11,7 +11,10 @@ register = template.Library()
 def ref_token(entity, pk):
     return "@{}:{}".format(entity, pk)
 def ref_pattern(entity):
-    return "@{}:(\d+)".format(entity)
+    if entity == 'user':
+        return "@{}:(\w+)".format(entity)
+    else:
+        return "@{}:(\d+)".format(entity)
 
 def story_replacement(storyId, user, autoescape=True):
     esc = conditional_escape if autoescape else lambda x: x
@@ -31,20 +34,20 @@ def book_replacement(bookId, user, autoescape=True):
     esc = conditional_escape if autoescape else lambda x: x
     try:
         book = Book.objects.get(pk=bookId)
-        replacement = '<a href="{}">{}</a>'.format(
-            reverse('show_book', args=[int(bookref)]),
+        return '<a href="{}">{}</a>'.format(
+            reverse('show_book', args=[bookId]),
             esc(book.title)
         )
     except Book.DoesNotExist: 
-        replacement = "a missing book"
+        return "a missing book"
 
-def user_replacement(userId, user, autoescape=True):
+def user_replacement(username, user, autoescape=True):
     esc = conditional_escape if autoescape else lambda x: x
     try:
-        if userId == user.id:
+        if username == user.username:
             return "you"
         else: 
-            subjectUser = User.objects.get(pk=userId)
+            subjectUser = User.objects.get(username=username)
             return '<a href="{}">{}</a>'.format(
                 reverse('show_user', args=[subjectUser.username]),
                 esc(subjectUser.username)
@@ -57,8 +60,8 @@ def linkstory(storyId, user, autoescape=True):
     return mark_safe(story_replacement(storyId, user))
 
 @register.filter(needs_autoescape=True)
-def linkuser(userId, user, autoescape=True):
-    return mark_safe(user_replacement(userId, user))
+def linkuser(username, user, autoescape=True):
+    return mark_safe(user_replacement(username, user))
 
 @register.filter(needs_autoescape=True)
 def linkrefs(text, user, autoescape=True):  
@@ -72,10 +75,9 @@ def linkrefs(text, user, autoescape=True):
         bookId = int(bookref)
         replacement = book_replacement(bookId, user, autoescape)
         replacements.append((ref_token('book', bookId), replacement))
-    for userref in re.findall(ref_pattern('user'), text):
-        userId = int(userref)
-        replacement = user_replacement(userId, user, autoescape)
-        replacements.append((ref_token('user', userId), replacement))
+    for username in re.findall(ref_pattern('user'), text):
+        replacement = user_replacement(username, user, autoescape)
+        replacements.append((ref_token('user', username), replacement))
 
     for old, new in replacements:
         text = text.replace(old, new)
