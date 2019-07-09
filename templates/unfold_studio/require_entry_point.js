6a71f8bb3232d623e49586c3d8995469bd8d9687
Chris Proctor <chris.proctor@gmail.com>
2019-07-09 13:44:18 -0400

    Bugfix: compilation was failing bc frontend urls had double-slash, getting 301 redirect without post params.

--- templates/unfold_studio/require_entry_point.js
+++ templates/unfold_studio/require_entry_point.js
@@ -10,17 +10,17 @@ require.config({
 
 define('fetch_story', ['jquery'], function($) {
     return function(story_id) {
-        return $.ajax("{% url 'list_stories' %}/" + story_id + '/json');
+        return $.ajax("{% url 'list_stories' %}" + story_id + '/json');
     }
 })
 
 define('save_story', ['jquery'], function($) {
     return function(story) {
         if (story.id) {
-            var url = "{% url 'list_stories' %}/" + story.id + '/compile';
+            var url = "{% url 'list_stories' %}" + story.id + '/compile';
         }
         else {
-            var url = "{% url 'list_stories' %}/new" 
+            var url = "{% url 'list_stories' %}new" 
         }
         return $.ajax(url, {
             beforeSend: function(xhr) { 
