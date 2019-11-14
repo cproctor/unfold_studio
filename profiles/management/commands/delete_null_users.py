bb61eff214ddea83edbbec4bc0c83e1ddb1f606f
Chris Proctor <chris.proctor@gmail.com>
2019-11-14 09:10:43 -0500

    Added delete null users command

---
+++ profiles/management/commands/delete_null_users.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.db.models import Count
+from django.utils import timezone
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = "Delete users who haven't logged in recently and who have no stories"
+
+    def add_arguments(self, parser):
+        parser.add_argument("-a", "--age", help="minimum age in days", default=90)
+        parser.add_argument("-d", "--dryrun", help="show users who would be deleted, but don't delete them", action="store_true")
+
+    def handle(self, *args, **options):
+        cutoff = timezone.now() - timedelta(days=options["age"])
+        null_users = User.objects.annotate(num_stories=Count("stories")).filter(num_stories=0, last_login__lt=cutoff)
+        if int(options["verbosity"]) > 1 or options["dryrun"]:
+            self.stdout.write("{} users {} be deleted:".format(null_users.count(), "would" if options["dryrun"] else "will"))
+            for user in null_users.all():
+                self.stdout.write(" - {}".format(user))
+        if not options["dryrun"]:
+            for user in null_users.all():
+                user.delete()
