2b17753e5130c3efe8de1b6b658be3665a5424cc
Chris Proctor <chris@chrisproctor.net>
2024-06-22 17:30:04 -0400

    Update base settings

---
+++ unfold_studio/unfold_studio/management/commands/dev_init.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.sites.models import Site
+from django.conf import settings
+
+template = """
+Created a development site with domain {domain}.
+Ensure that /etc/hosts has the following line:
+
+    127.0.0.1   {domain}
+
+Make sure that settings.py has the following setting:
+
+    SITE_ID = {site.id}
+"""
+
+class Command(BaseCommand):
+    help = "Populate the database with initial values for development"
+
+    def add_arguments(self, parser):
+        parser.add_argument("-d", '--domain', default="local.unfoldstudio.net")
+        parser.add_argument("-n", '--name', default="Dev Unfold Studio")
+        parser.add_argument("-f", '--force', action="store_true")
+
+    def handle(self, *args, **options):
+        site_count = Site.objects.count()
+        if site_count:
+            if options['force']:
+                Site.objects.all().delete()
+            else:
+                raise CommandError(f"This command would destroy {site_count} existing Sites and related content.")
+        site = Site.objects.create(
+            id=1, 
+            domain=options['domain'],
+            name=options['name'],
+        )
+        if getattr(settings, 'SITE_ID') != site.id:
+            print({"site": site, **options})
+            print(template.format(site=site, **options))
