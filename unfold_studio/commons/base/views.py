9373910a891424a419142d7fb6d8f0f4561b318e
Asif Ahmed <88107284+asifahmed70707@users.noreply.github.com>
2025-03-05 22:23:52 -0500

    replace authenticated view with base view (#193)
    Co-authored-by: Asif Ahmed <asifahmed@Asifs-MacBook-Air-5.local>

--- unfold_studio/commons/base/views.py
+++ unfold_studio/commons/base/views.py
@@ -2,8 +2,11 @@ from django.views import View
 from django.contrib.auth.mixins import AccessMixin
 from django.http import JsonResponse
 
+class BaseView(AccessMixin, View):
+    pass
 
-class AuthenticatedView(AccessMixin, View):
+
+class AuthenticatedView(BaseView):
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return JsonResponse(
