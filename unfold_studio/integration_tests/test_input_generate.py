beb13ddc12a0c01dedd2cf935922bfb074427206
Asif Ahmed <asifahmed@Asifs-MacBook-Air-5.local>
2025-04-03 18:28:19 -0400

    add cicd

--- unfold_studio/integration_tests/test_input_generate.py
+++ unfold_studio/integration_tests/test_input_generate.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import re
+import os
 
 LIGHT_GREEN = '\033[38;5;120m'
 DEEP_GREEN = '\033[32m'
@@ -362,6 +363,20 @@ def test_all_paths():
     chrome_options = Options()
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--disable-browser-side-navigation')
+    chrome_options.add_argument('--disable-features=VizDisplayCompositor')
+    chrome_options.add_argument('--window-size=1920,1080')
+    
+    # Use system Chrome binary if available
+    if os.environ.get('CHROME_BIN'):
+        chrome_options.binary_location = os.environ.get('CHROME_BIN')
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.maximize_window()
