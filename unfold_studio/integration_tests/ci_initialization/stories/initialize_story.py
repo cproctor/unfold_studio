9acc4681a2baddcc099ffdd5e0a69d5f13123b26
Asif Ahmed <asifahmed@Asifs-MacBook-Air-5.local>
2025-04-04 15:30:04 -0400

    add cicd

--- unfold_studio/integration_tests/ci_initialization/stories/initialize_story.py
+++ unfold_studio/integration_tests/ci_initialization/stories/initialize_story.py
@@ -2,22 +2,7 @@ import reversion
 from unfold_studio.models import Story
 import django.utils.timezone as timezone
 
-def create_story(user, site, story_id, title, ink_content, description="Test story", is_public=True):
-    """
-    Generic function to create a story with given parameters.
-    
-    Args:
-        user: The user who will be the author of the story
-        site: The site to associate the story with
-        story_id: The ID to assign to the story
-        title: The title of the story
-        ink_content: The Ink content/template of the story
-        description: Story description (default: "Test story")
-        is_public: Whether the story should be public (default: True)
-    
-    Returns:
-        int: The ID of the created story
-    """
+def create_story(user, site, story_id, title, ink_content, description, is_public=True):
     story = Story.objects.create(
         id=story_id,
         title=title,
@@ -26,9 +11,9 @@ def create_story(user, site, story_id, title, ink_content, description="Test sto
         public=is_public,
         creation_date=timezone.now(),
         edit_date=timezone.now(),
-        description=description
+        description=description,
+        sites=[site],
     )
-    story.sites.add(site)
     
     # Save the story first to get a version
     story.save()
