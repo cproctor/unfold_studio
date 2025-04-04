01f6133b41239c00638ec94d413c769c5ceb8f7e
Asif Ahmed <asifahmed@Asifs-MacBook-Air-5.local>
2025-04-03 20:01:47 -0400

    add cicd

--- unfold_studio/integration_tests/initialization.py
+++ unfold_studio/integration_tests/initialization.py
@@ -1,6 +1,7 @@
 import os
 import django
 from django.utils import timezone
+import reversion
 
 # Set up Django environment first
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unfold_studio.settings')
@@ -27,27 +28,27 @@ def initialize_test_environment():
         id=29,
         title='Test Story',
         ink='''
-VAR name = """"
-VAR food = """"
-VAR color = """"
-VAR number = """"
-VAR story = """"
+VAR name = ""
+VAR food = ""
+VAR color = ""
+VAR number = ""
+VAR story = ""
 
 This is a test story for input and generate functionality.
 
 Let's start with a simple input.
-~ input(""What's your name?"", ""name"")
+~ input("What's your name?", "name")
 BUG
 Nice to meet you, {name}!
 
 Now let's test generate with some context.
-~ generate(""Write a short greeting for {name}"")
+~ generate("Write a short greeting for {name}")
 
 Let's try input after generate.
-~ input(""What's your favorite food?"", ""food"")
+~ input("What's your favorite food?", "food")
 BUG
 I see you like {food}. Let's generate something about that.
-~ generate(""Write a short description about why {name} might like {food} in 20 words"")
+~ generate("Write a short description about why {name} might like {food} in 20 words")
 
 Now let's test input with choices.
 What would you like to do next?
@@ -60,27 +61,27 @@ What would you like to do next?
     -> skip_choice
 
 === color_choice ===
-~ input(""What's your favorite color?"", ""color"")
+~ input("What's your favorite color?", "color")
 BUG
 {color} is a great choice!
 
 What would you like to do with this color?
 
 + Generate something about the color
-    ~ generate(""Write a short poem about the color {color} in 20 words"")
+    ~ generate("Write a short poem about the color {color} in 20 words")
     -> end
 + Skip generation
     -> end
 
 === number_choice ===
-~ input(""Pick a number between 1 and 10"", ""number"")
+~ input("Pick a number between 1 and 10", "number")
 BUG
 You chose {number}.
 
 What would you like to do with this number?
 
 + Generate something about the number
-    ~ generate(""Write a short story about the number {number} in 20 words"")
+    ~ generate("Write a short story about the number {number} in 20 words")
     -> end
 + Skip generation
     -> end
@@ -101,7 +102,14 @@ The end!
     )
     story.sites.add(site)
     
-    # Compile the story
+    # Save the story first to get a version
+    story.save()
+    
+    # Register the story with reversion
+    with reversion.create_revision():
+        reversion.add_to_revision(story)
+    
+    # Now compile the story
     story.compile()
     story.save()
     
