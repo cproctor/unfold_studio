0047d0ac48ac560a83040f2b558a7162c15bf965
Asif Ahmed <asifahmed@Asifs-MacBook-Air-5.local>
2025-04-05 13:31:13 -0400

    add cicd

--- unfold_studio/integration_tests/ci_initialization/initialize_test_environment.py
+++ unfold_studio/integration_tests/ci_initialization/initialize_test_environment.py
@@ -8,8 +8,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
-from integration_tests.ci_initialization.text_generation.initialize_text_generation_records import initialize_text_generation_records
-from integration_tests.ci_initialization.stories.initialize_input_generate_story import create_input_generate_story
+from unfold_studio.integration_tests.ci_initialization.text_generation.initialize_text_generation_records import initialize_text_generation_records
+from unfold_studio.integration_tests.ci_initialization.stories.initialize_input_generate_story import create_input_generate_story
 
 def create_test_user():
     return User.objects.create_user('testuser', 'test@example.com', 'testpass')
