e4f747887fe6ccbf584345b8a5016d748481fbeb
Chris Proctor <chris@chrisproctor.net>
2025-11-07 16:33:46 -0500

    Reimplemented example sampling

--- unfold_studio/text_generation/management/commands/generate_continue_examples.py
+++ unfold_studio/text_generation/management/commands/generate_continue_examples.py
@@ -9,24 +9,17 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-n', '--num-examples', type=int, default=100, 
                 help="Number of examples to generate")
-        parser.add_argument('-s', "--story", type=int, nargs='+', 
-                help="id(s) of stories to use for examples")
-        parser.add_argument('-t', '--num-turn-sequences-per-story', type=int, default=0, 
-                help="Max number of story plays to generate per story")
-        parser.add_argument('-u', '--num-stories', type=int, default=0, 
-                help="Number of stories to use in queryset")
         parser.add_argument('-o', "--output", help="file to save result in CSV format")
 
     def handle(self ,*args, **options):
         verbose = options['verbosity'] > 1
         model = ContinueClassificationModel()
         queryset = model.get_default_queryset()
-        if options['story']:
-            queryset = queryset.filter(id__in=options['story'])
-        if options['num_stories']:
-            queryset = queryset.order_by("?")[:options['num_stories']]
-        examples = model.generate_examples(n=options['num_examples'], queryset=queryset, 
-                turn_sequences_per_story=options['num_turn_sequences_per_story'], verbose=verbose)
+        examples = model.generate_examples(
+            queryset=queryset, 
+            n=options['num_examples'],
+            verbose=verbose
+        )
         if options['output']:
             with open(options['output'], 'w') as fh:
                 writer = csv.writer(fh)
