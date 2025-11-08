8b83adc7af4a6c6eef2f54b3471dbe48db9692f0
Chris Proctor <chris@chrisproctor.net>
2025-11-08 05:48:52 -0500

    Add threading

--- unfold_studio/text_generation/management/commands/evaluate_continue_examples.py
+++ unfold_studio/text_generation/management/commands/evaluate_continue_examples.py
@@ -9,12 +9,14 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('examples', help="File path for examples in CSV format")
         parser.add_argument('-e', "--errors", help="File path to save error analysis as CSV")
+        parser.add_argument('-p', "--max-parallel-requests", type=int, default=8, 
+                help="Maximum number of parallel requests for classification")
 
     def handle(self ,*args, **options):
         with open(options["examples"]) as fh:
             examples = list(csv.reader(fh))
         model = ContinueClassificationModel()
-        model.evaluate(examples)
+        model.evaluate(examples, max_parallel_requests=options["max_parallel_requests"])
         model.report_evaluation_results()
         if options["errors"]:
             model.report_error_analysis()
