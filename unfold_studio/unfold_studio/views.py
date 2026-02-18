0d89ce90852046dd1b5ec4f7023d4cc4c886ee8e
mukeshkrishnamurthy <mukeshkr@buffalo.edu>
2026-02-17 20:21:08 -0500

    Task #83: Extend story search to include authors

--- unfold_studio/unfold_studio/views.py
+++ unfold_studio/unfold_studio/views.py
@@ -73,7 +73,7 @@ def browse(request):
             stories = stories.annotate(
                 rank=SearchRank(F('search'), query), 
                 score=F('rank') * F('priority') / (F('rank') + F('priority'))
-            ).filter(rank__gte=s.SEARCH_RANK_CUTOFF).order_by('-score')
+            ).filter(Q(rank__gte=s.SEARCH_RANK_CUTOFF)|Q(author__username__icontains=form.cleaned_data['query'])).order_by('-score')
         else:
             messages.warning(request, "Please enter a valid search query")
             return redirect('list_stories')
