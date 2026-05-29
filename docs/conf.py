243eecad11cc1ce691efdb243d356ecd0073630d
Chris Proctor <chrisp@buffalo.edu>
2026-05-28 23:04:05 -0400

    Reorganize design docs: docs/refactor -> docs/design/2.0_refactor

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,6 @@ release = '1.0'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'refactor']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'design']
 
 html_theme = 'sphinx_rtd_theme'
