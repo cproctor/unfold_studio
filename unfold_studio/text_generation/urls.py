7c9354543211dd8bd6b60d9e0c6c228a49d32458
Chris Proctor <chrisp@buffalo.edu>
2026-05-29 18:47:05 -0400

    Remove incomplete AgentView: constants AGENT_CHARACTER_SYSTEM_PROMPT and AGENT_CHARACTER_USER_PROMPT_TEMPLATE were never added to constants.py

--- unfold_studio/text_generation/urls.py
+++ unfold_studio/text_generation/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('generate/', views.GenerateTextView.as_view(), name="generate"),
-    path('get_next_direction/', views.GetNextDirectionView.as_view(), name="get_next_direction"),
-    path('agent/', views.AgentView.as_view(), name="agent")
+    path('get_next_direction/', views.GetNextDirectionView.as_view(), name="get_next_direction")
 ]
 
